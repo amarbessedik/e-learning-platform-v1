9c3488f1c0e8b74b4b359b2d883cf24c2855fb1d
amarbessedik <amar.bessedik@gmail.com>
2020-10-18 21:40:32 -0400

    Sign In & Register Pages added

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,11 +1,66 @@
 import React from 'react';
 import './Register.css';
+import AppleIcon from "@material-ui/icons/Apple";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
 
 const Register = () => {
     return (
-        <div className='register__page'>
-            <h1>Register Page</h1>
+      <div className="register__page">
+        <div className="register__header__wrapper">
+          <div className="register__header">
+            <p>
+              Already have an eLearn account?
+              <a className="help__link__register" href="/signin">
+                Sign In
+              </a>
+            </p>
+          </div>
         </div>
+        <div className="register__form__wrapper">
+          <h2>Create a new account here</h2>
+          <form>
+            <input type="text" placeholder="Full Name" />
+            <input type="text" placeholder="Public Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Country or Region of Residence" />
+            <button className="register__btn" type="submit">
+              Create Account
+            </button>
+          </form>
+          <p>
+            By creating an account, you agree to the{" "}
+            <a className="help__link" href="/">
+              Terms of Service and Honor Code
+            </a>{" "}
+            and you acknowledge that edX and each Member process your personal
+            data in accordance with the{" "}
+            <a className="help__link" href="/">
+              Privacy Policy
+            </a>
+            .
+          </p>
+        </div>
+        <div className="register__social__media">
+          <h2>Or create an account using</h2>
+          <div className="register__btns">
+            <button className="social__media__btn apple">
+              <AppleIcon className="social__media__icon" /> Apple
+            </button>
+            <button className="social__media__btn facebook">
+              <FacebookIcon className="social__media__icon" /> Facebook
+            </button>
+            <button className="social__media__btn github">
+              <GitHubIcon className="social__media__icon" /> Github
+            </button>
+            <button className="social__media__btn twitter">
+              <TwitterIcon className="social__media__icon" /> Twitter
+            </button>
+          </div>
+        </div>
+      </div>
     );
 }
 
--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,11 +1,53 @@
 import React from 'react';
 import './SignIn.css';
+import AppleIcon from "@material-ui/icons/Apple";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
 
 const SignIn = () => {
     return (
-        <div className='signin__page'>
-            <h1>Sign In Page</h1>
+      <div className="signin__page">
+        <div className="signin__header">
+          <p>
+            First time here? <a className='help__link' href="/register">Create an Account</a>{" "}
+          </p>
         </div>
+        <div className="signin__credentials">
+          <h2>Sign In</h2>
+          <br />
+          <form>
+            <label htmlFor="email">Email</label>
+            <input name="email" type="email" placeholder="Enter your email" />
+            <label htmlFor="password">Password</label>
+            <input 
+              name="password"
+              type="password"
+              placeholder="Enter your password"
+            />
+            <button className='signin__btn' type="submit">Sign in</button>
+          </form>
+        <a className='help__link' href="/">Need help signing in?</a>
+        <a className='help__link' href="/">Sign in with your company or school</a>
+        </div>
+        <div className="signin__social__media">
+          <h2>or sign in with</h2>
+          <div className='signin__btns'>
+            <button className="social__media__btn apple">
+              <AppleIcon className="social__media__icon"/> Apple
+            </button>
+            <button className="social__media__btn facebook">
+              <FacebookIcon className="social__media__icon"/> Facebook
+            </button>
+            <button className="social__media__btn github">
+              <GitHubIcon className="social__media__icon"/> Github
+            </button>
+            <button className="social__media__btn twitter">
+              <TwitterIcon className="social__media__icon"/> Twitter
+            </button>
+          </div>
+        </div>
+      </div>
     );
 }
 
